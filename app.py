a360379df3f59628f9c10a68e307baa6c0f0e2fc
felipemaxferreira <felipemax@gmail.com>
2025-11-05 00:58:23 -0300

    Alterações em dados

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def left_panel():
 def chat_panel():
     inject_global_css()
     
-    st.markdown("#### Chat com o Bino")
+    st.markdown("#### Assistente com o Bino")
     
     # Container principal do chat
     st.markdown('<div class="bino-chat-main">', unsafe_allow_html=True)
